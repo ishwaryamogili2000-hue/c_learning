1c4812bbaa5095defa577eff215e0b7f82870941
frndu <git@gmail.com>
2026-03-09 21:32:21 +0530

    rev of num code

---
+++ rev_num.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = 234;
+    int rev =0,digit =0;
+
+    while(num>0)
+    {
+        digit = num%10;
+        rev = rev *10 +digit;
+        num = num/10;
+        
+    }
+     printf("rev of num %d\n",rev);
+   
+}
